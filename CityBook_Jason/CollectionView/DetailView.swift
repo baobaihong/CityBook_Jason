19845a1b830376731b9ab5e5d5e9fcba89b00217
Jason Bao <baihongbao@outlook.com>
2023-12-18 16:40:53 +0800

    feat: add back button for poi card view, refine detail view of collectionview

---
+++ CityBook_Jason/CollectionView/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  CityBook_Jason
+//
+//  Created by Jason on 2023/12/18.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
